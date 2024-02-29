ad447d27ded200246509cf2b0c838760c6aeab0d
Armaghan Saqib <ar.saqib@Gmail.com>
2024-02-29 20:00:01 +0500

    Changes done on 2024-02-29

--- index.php
+++ index.php
@@ -712,25 +712,6 @@ if($mobile->isMobile()) {
 
                 <div class="item">
                     <div class="news-box">
-                        <a href="news-2459-aitchison-and-sadiq-public-school-enjoy-the-annual-sports-carnival" target="_blank">
-                            <div class="news-image">
-                                <img src="newsletter/news/images/22-1645206665.jpg" alt="News" class="img-fluid"/>
-                            </div>
-                            <div class="news-contents" style="padding: 1rem;">
-                                <div class="news-date" style="margin: 5px 0;">
-                                    29<sup>th</sup> January, 2024
-                                </div>
-                                <h3 class="news-heading">
-                                    Aitchison and Sadiq Public School Enjoy the Annual Sports Carnival
-                                </h3>
-
-                            </div>
-                        </a>
-                    </div>
-                </div>
-
-                <div class="item">
-                    <div class="news-box">
                         <a href="aitchison-senior-mathematics-society-delegation-bags-medals-at-seamo-x-2024" target="_blank">
 
                             <div class="news-image">

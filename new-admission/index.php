f33c0f6b1cf94a00b94ecd895371a0712ad98ac3
Armaghan Saqib <ar.saqib@Gmail.com>
2024-07-01 20:00:02 +0500

    Changes done on 2024-07-01

--- new-admission/index.php
+++ new-admission/index.php
@@ -94,7 +94,7 @@ $page_header = $title;
         list-style: none;
     }
     .add-menu-item {
-        padding: 0 5px;
+        padding: 0 10px;
     }
     .add-menu-item a {
         color: var(--color-blue);
@@ -102,7 +102,7 @@ $page_header = $title;
 
     .add-menu-item::before {
         content: '';
-        border-right: 1px solid;
+        /*border-right: 1px solid;*/
 
     }
 
@@ -154,6 +154,25 @@ $page_header = $title;
     .policy-process h2 {
         font-weight: 200;
 
+    }
+
+    .senior-admission {
+        padding: 7rem 0;
+    }
+
+    .age-guidelines {
+        padding-top: 4rem;
+        background: linear-gradient(var(--color-yellow) 65%, transparent 35%);
+    }
+
+    .age-guidelines h2 {
+        color: var(--color-blue)
+    }
+    .age-guidelines-carousel .item {
+        background-color: #fff;
+        text-align: center;
+        border: 2px solid var(--color-blue);
+        padding: 3rem 2rem;
     }
 </style>
 
@@ -240,43 +259,76 @@ $page_header = $title;
     </div>
 </div>
 
-<div class="container">
-    
-    <!-- Contents -->
-    <div class="row page-contents">
-
-        <!-- Left Area -->
-        <div class="col-lg-9 col-md-12 col-sm-12">
-            <p>Aitchison College admits boys on the basis of academic and general suitability. The application process must be completed before a boy is considered an eligible candidate. Applications may only be made for one of the two enrollment categories (boarding or day) and not both. Entries into Junior, Prep and Senior are treated separately for admission purposes. The College reserves the right to offer additional places to eligible students, at any time and at any year level, in order to reach full capacity. A boy who is offered a place will have completed competitive admission tests and been interviewed before a final decision is made by the Principal. Age criteria are strictly&nbsp;observed and no age waivers will be given. <b>Over-age applicants will not be considered.</b> No boy may appear more than once in a testing cycle. An offer of admission will also be automatically withdrawn if an application, transcript, reference or age related document has been falsified in any way whatsoever.&nbsp;</p>
-
-            <p><strong>Test papers and results&nbsp;are not shared and nor does the College discuss the performance of individual applicants. This is a condition of application.</strong>&nbsp;</p>
-
-            <p>Boys who have been schooled in Lahore are normally ineligible to apply for a boarding place. Please direct your enquiries to the HR, Ext: 281.&nbsp;<strong>Aitchison&#39;s boarding is on a national scale.&nbsp;Conditional and limited bursaries are available to boys who qualify for admission from Baluchistan, FATA, Sindh, KPK, Azad Kashmir, Gilgit-Baltistan and Southern Punjab.</strong></p>
-
-        </div>
-        <!-- End Left Area -->
-
-        <!-- Right Area -->
-        <div class="col-lg-3 col-md-6 col-sm-12 text-center news-links">
-            <?php include_once ('right-panel-links.php'); ?>
-        </div>
-        <!-- End Right Area -->
-
-        <div class="col-lg-9 col-md-12 col-sm-12">
-
-            <div class="divider"></div>
-
-            <div class="row">
-
+<div class="container-fluid senior-admission">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <img src="images/image-2.jpg" alt="image" class="img-fluid" />
+            </div>
+            <div class="col-6">
+                <h2>SENIOR ADMISSIONS TO AITCHISON COLLEGE 2024-25<hr style="margin-left: 0;"/></h2>
+                <div>
+                    <p><strong>Senior School</strong>&nbsp;entry to H1 (to study Cambridge AS Level or FBISE FSc&nbsp;(Pre-Medical) for day and boarding)&nbsp;accepted from <strong>May 1<sup>st</sup> to June 30<sup>th</sup>, 2024.</strong>&nbsp; Limited places for Internationals are based on Cambridge or other acceptable curriculum.&nbsp;</p>
+                    <p>Parents seeking a place for their son(s) should first read the&nbsp;<a href="https://aitchison.edu.pk/admission/admissions-info" style="color: blue;">Admissions</a>&nbsp;section of the College's website, where our policy and processes are explained, along with entry prerequisites for various entry levels. Parents can also access our&nbsp;<a href="https://aitchison.edu.pk/admission/admissions-faqs" style="color: blue;">FAQs</a><em>.</em>&nbsp;If there remain further queries, please contact our HR Department on Extension: 281&nbsp;or email:&nbsp;<span style="color: blue;"><strong>admission@aitchison.edu.pk.</strong></span> Given the very high interest in Aitchison, the College does not offer tours or admission appointments. Our website is particularly informative and visually&nbsp;helpful.</p>
+                    <p>Applications may be <a href="https://aitchison.edu.pk/admission/a-level-fsc-application-forms" target="_parent" style="color: blue;">downloaded</a> from our Admissions section and mailed to the College Admissions Office, Shahrah-e-Quaid-e-Azam, Lahore 54000 Pakistan, Ph: 0092-42-36317201-04. Your registration will be acknowledged as soon as possible. Please complete the Application Form accurately.</p>
+                </div>
             </div>
         </div>
+    </div>
+</div>
 
-        <div class="col-lg-3 col-md-12 col-sm-12">
+<div class="container-fluid age-guidelines">
 
+    <div class="container">
+        <h2 class="text-center">AGE GUIDELINES<br>FOR ADMISSIONS 2024-25<hr style="border-color:var(--color-blue);"/></h2>
+        <div class="age-guidelines-carousel owl-carousel owl-theme">
+            <div class="item">
+                <h2>Year 1 (K1)</h2>
+                <p class="fw-bold">Date of Birth</p>
+                <p>June 1, 2018 – May 31, 2019 inclusive</p>
+            </div>
+            <div class="item">
+                <h2>Year 2 (K2)</h2>
+                <p class="fw-bold">Date of Birth</p>
+                <p>June 1, 2017 – May 31, 2018 inclusive</p>
+            </div>
+            <div class="item">
+                <h2>Year 6 (K6)</h2>
+                <p class="fw-bold">Date of Birth</p>
+                <p>June 1, 2013 – May 31, 2014 inclusive</p>
+            </div>
+            <div class="item">
+                <h2>Year 7 (E2)</h2>
+                <p class="fw-bold">Date of Birth</p>
+                <p>June 1, 2012 – May 31, 2013 inclusive</p>
+            </div>
+            <div class="item">
+                <h2>Year 7 (E2)</h2>
+                <p class="fw-bold">Date of Birth</p>
+                <p>June 1, 2012 – May 31, 2013 inclusive</p>
+            </div>
+            <div class="item">
+                <h2>Year 8 (M1)</h2>
+                <p class="fw-bold">Date of Birth</p>
+                <p>June 1, 2011 – May 31, 2012 inclusive</p>
+            </div>
+            <div class="item">
+                <h2>Year 9 (M2)</h2>
+                <p class="fw-bold">Date of Birth</p>
+                <p>June 1, 2010 – May 31, 2011 inclusive</p>
+            </div>
+            <div class="item">
+                <h2>Year 10 (C1)</h2>
+                <p class="fw-bold">O Level/Matric (First Year)</p>
+                <p>June 1, 2009 – May 31, 2010 inclusive</p>
+            </div>
+            <div class="item">
+                <h2>Year 12 (H1)</h2>
+                <p class="fw-bold">A Level (First Year)</p>
+                <p>June 1, 2007 – May 31, 2008 inclusive</p>
+            </div>
         </div>
     </div>
-    <!-- End Contents -->
-
 </div>
 
 <div class="divider"></div>
@@ -295,5 +347,33 @@ $page_header = $title;
 
 
 </body>
+<script>
+    $(document).ready(function () {
+        $('.age-guidelines-carousel').owlCarousel({
 
+            singleItem: true,
+            animateOut: 'slideOutDown',
+            animateIn: 'flipInX',
+
+            loop:true,
+            margin:30,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: false,
+            nav:true,
+            dots:false,
+            items:3,
+
+            stagePadding:30,
+            smartSpeed:450,
+
+            //animateOut: 'slideOutUp',
+
+            //dots:false,
+            navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        });
+    });
+
+
+</script>
 </html>
